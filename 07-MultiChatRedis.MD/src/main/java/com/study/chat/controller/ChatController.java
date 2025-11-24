3c92f75e26596cbb422d07264e0d9ccf567d5eae
hartli <463644535@qq.com>
2025-11-24 19:10:18 +0800

    feat: Chapter 8

--- 07-MultiChatRedis.MD/src/main/java/com/study/chat/controller/ChatController.java
+++ 07-MultiChatRedis.MD/src/main/java/com/study/chat/controller/ChatController.java
@@ -7,7 +7,6 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.util.StringUtils;
@@ -64,31 +63,4 @@ public class ChatController {
         return responseText;
     }
 
-    @GetMapping("/ai/chat/deepseek/param")
-    public String deepSeekParam(@RequestParam(value = "question") String question,
-                                @RequestParam(value = "conversationId") String conversationId) {
-        // 1. 生成会话ID（使用session id 确保用户隔离）
-//        String conversationId = session.getId();
-        // 2. 初始化系统消息
-        Message systemMessage = new SystemMessage("你是一名Java架构师，擅长精准而简洁的回答问题");
-        if (chatMemory.get(conversationId).isEmpty()) {
-            chatMemory.add(conversationId, systemMessage);  // 添加至对话记忆
-        }
-        // 3. 手动获取历史消息
-        List<Message> historyMessages = chatMemory.get(conversationId);
-        // 4.用户消息
-        Message userMessage = new UserMessage(question);
-        // 新建集合，避免污染历史消息
-        List<Message> promptMessages = new ArrayList<>(historyMessages);
-        // 本次用户消息合并到历史消息中
-        promptMessages.add(userMessage);
-        // 5. 完成 Prompt 对象
-        Prompt prompt = new Prompt(promptMessages);
-        // 6. 发送至 AI 模型，提取响应文本
-        String responseText = chatClient.prompt(prompt).call().content();
-        // 7. 本次 AI 响应添加至对话记忆（助手角色）
-        chatMemory.add(conversationId, new AssistantMessage(responseText));
-        // 8. 返回响应文本
-        return responseText;
-    }
 }
---
+++ 8-Advisor/src/main/java/com/study/chat/config/SpringAIConfig.java
@@ -0,0 +1,41 @@
+package com.study.chat.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author HartLi
+ * @version 1.0.0
+ * @desc TODO
+ * @date 2025-11-24 11:44
+ */
+@Configuration
+public class SpringAIConfig {
+
+    @Resource
+    private RedisChatMemoryRepository redisChatMemoryRepository;
+
+    // 创建基于默认的大模型的客户端
+    @Bean
+    public ChatClient openaiChatClient(ChatClient.Builder builder) {
+        return builder.defaultAdvisors(
+                MessageChatMemoryAdvisor.builder(chatMemory(redisChatMemoryRepository)).build())
+            .defaultSystem(
+                system -> system.text("你是一名{role}，擅长精准而简洁得回答问题")
+                    .param("role", "Java架构师")).build();
+    }
+
+    // 创建特定的 ChatMemory实例
+    @Bean
+    public ChatMemory chatMemory(RedisChatMemoryRepository redisChatMemoryRepository) {
+        return MessageWindowChatMemory.builder()
+            .chatMemoryRepository(redisChatMemoryRepository) // 对话记忆使用基于 Redis 的存储库
+            .maxMessages(10) // 保留最近的 10 条历史记录
+            .build();
+    }
+}
