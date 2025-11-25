7359892390e35fe99b4dced1854f89e973e50dbe
hartli <463644535@qq.com>
2025-11-25 17:59:50 +0800

    feat: Chapter 13

---
+++ 13-ToolCall/src/main/java/com/study/chat/controller/ChatController.java
@@ -0,0 +1,44 @@
+package com.study.chat.controller;
+
+import com.study.chat.tools.DateTimeTools;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HartLi
+ * @version 1.0.0
+ * @desc TODO
+ * @date 2025-11-18 15:51
+ */
+@RestController
+public class ChatController {
+
+    @Resource
+    private ChatClient chatClient;
+    @Resource
+    private DateTimeTools dateTimeTools;
+
+    @GetMapping("/ai/chat/deepseek")
+    public String deepSeek(@RequestParam(value = "question") String question) {
+        // 请求模型并转换为对应的实体
+        return chatClient.prompt()
+            .user(question)
+            .call()
+            .content();
+    }
+
+    @GetMapping("/ai/chat/tools")
+    public String chatWithTools(@RequestParam(value = "question") String question) {
+        // 请求模型并转换为对应的实体
+        return chatClient.prompt()
+            .user(question)
+            .tools(dateTimeTools)
+            .call()
+            .content();
+    }
+
+
+}
